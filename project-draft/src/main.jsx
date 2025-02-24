461d71b23872b1eb0c387f5adc9af1f371dc871c
brandonjinkim <brandonjinkim@gmail.com>
2025-02-24 13:35:44 -0800

    Setup main.jsx and App.jsx pages

--- project-draft/src/main.jsx
+++ project-draft/src/main.jsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(<App />);
+import { BrowserRouter } from 'react-router';
+
+import './index.css'
+
+import App from './components/App.jsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
