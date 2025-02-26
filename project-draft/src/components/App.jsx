0d31d35a5c7e3b7ded04ae2542b2494e8a469a26
brandonjinkim <brandonjinkim@gmail.com>
2025-02-26 12:20:51 -0800

    Setup routes

--- project-draft/src/components/App.jsx
+++ project-draft/src/components/App.jsx
@@ -3,13 +3,18 @@ import { Routes, Route } from 'react-router';
 import { Navbar } from './Navbar.jsx';
 import { Footer } from './Footer.jsx';
 import { HomePage } from './StaticPages.jsx';
+import { SignIn } from './signIn.jsx';
+
 
 function App(props) {
     return (
       <div>
         <Navbar />
         <main>
-            <HomePage />
+          <Routes>
+            <Route path="/" element = {<HomePage />} />
+            <Route path="/login" element = {<SignIn />} />
+          </Routes>
         </main>
         <Footer />
       </div>
--- project-draft/src/components/signIn.jsx
+++ project-draft/src/components/signIn.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
-export function Signin(props) {
+export function SignIn(props) {
 
     // State variables here
 
     return (
-    <div class="container">
-            <p class="sign-up">Sign in</p>
+        <div className="container">
+                <p className="sign-up">Sign in</p>
 
-            <div class="user-input">
-                <label for="email"></label><input id="email" type="email" name="email" placeholder="Email or Phone"/>
-            </div>
-            <div class="user-input">
-                <label for="password"></label><input id="password" type="password" name="password" placeholder="Password"/>
-            </div>
-            <div>
-                <a href="#" class="forgot-password">Forgot Password?</a>
-            </div>
-            <button class="sign-in">
-                <p>Sign In</p>
-            </button>
+                <div className="user-input">
+                    <label for="email"></label><input id="email" type="email" name="email" placeholder="Email or Phone"/>
+                </div>
+                <div className="user-input">
+                    <label for="password"></label><input id="password" type="password" name="password" placeholder="Password"/>
+                </div>
+                <div>
+                    <a href="#" className="forgot-password">Forgot Password?</a>
+                </div>
+                <button className="sign-in">
+                    <p>Sign In</p>
+                </button>
 
-            <p class="or">------ or ------</p>
+                <p className="or">------ or ------</p>
 
-            <button class="register">
-                <p>Register now!</p>
-            </button>
-    </div>
+                <button className="register">
+                    <p>Register now!</p>
+                </button>
+        </div>
     );
 }
 
