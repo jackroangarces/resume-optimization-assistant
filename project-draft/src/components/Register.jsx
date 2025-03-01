5a3b4ae5fad0bfca5d60973ad8a3765a6e705050
dangwilly <willdang@uw.edu>
2025-03-01 00:54:43 -0800

    added functioning username & pw functions

---
+++ project-draft/src/components/Register.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+export function Register(props) {
+
+
+    return (
+<p> e </p>
+    );
+}
--- project-draft/src/components/signIn.jsx
+++ project-draft/src/components/signIn.jsx
@@ -7,10 +7,28 @@ export function SignIn(props) {
     // const [password, setPassword] = useState(' '); */}
     const {login} = props;
     const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const registeredUsers = [{username:'redwall4lif', password:'seahawks48'}];
+   
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        login(username); // will add conditionals for invalid usernames here
+
+        let usernameInput = document.querySelector('.usernameInput').value;
+        let passwordInput = document.querySelector('.passwordInput').value;
+        let user = registeredUsers.find(user => user.username == usernameInput && user.password == passwordInput)
+
+        // will add conditionals for invalid usernames here
+
+        if (user) {
+            setError('');
+            login(user => username);
+        } else {
+            setError('Username not found, please enter a valid username.');
+        }
+        setUsername('');
+        setPassword('');
     }
 
 
@@ -18,12 +36,23 @@ export function SignIn(props) {
         <div className="container">
                 <p className="sign-up">Sign in</p>
                 <form onSubmit={handleSubmit}>
-                <div className="user-input">
+                <div className="user-input column">
+                    {<p style={{color: 'red'}}>{error}</p>}
                     <input 
+                        className="usernameInput"
                         type="text" 
                         placeholder="Enter Username" 
                         value={username} 
                         onChange={(event) => setUsername(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <input 
+                        className="passwordInput"
+                        type="text"
+                        placeholder="Enter password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
                     />
                 </div>
                 <button type="submit" className="sign-in">
